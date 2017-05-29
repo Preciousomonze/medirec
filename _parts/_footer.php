88ce5643cd633c358fc23dac875626b0bf8409fb
Omonzejele Precious <crispusomonze@gmail.com>
2017-05-29 13:09:02 +0100

    Delete _footer.php

--- _parts/_footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-
-<script type="text/javascript" src="<?php base_url("assets/js/bootstrap.min.js"); ?>"></script>
-<script type="text/javascript" src="<?php base_url("assets/js/main.js"); ?>"></script>
-
-</body>
-</html>
